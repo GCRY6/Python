8c1e67ef396ebabe782ca0300095f399c536d646
GCRY6 <2381165671@qq.com>
2025-03-15 22:17:29 +0800

    pingfang

--- ji he.py
+++ ji he.py
@@ -7,14 +7,15 @@ def fenkai():
 
 def pingfang():
     a=input("请输入：")
-    b=len(a)
+    a=a.split()
+    a=[int(num) for num in a]
     d=0
-    for i in range(b):
+    for i in range(len(a)):
         f=a[i]
         if f % 2==0:
-            c=a**a
-            e=d+c
-    print(e)
+            c=f*f
+            d+=c
+    print(d)
 
 if gongnen=="fenkai":
     fenkai()        
