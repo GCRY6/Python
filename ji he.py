5c87b5ca23b48060f873064fbbae5603da5bf2a9
GCRY6 <2381165671@qq.com>
2025-03-15 19:16:53 +0800

    zeng tian

--- ji he.py
+++ ji he.py
@@ -1,8 +1,19 @@
-def fnakai():
-    a=input("Enter: ")
+gongnen=input("你好，请问你想要什么功能？")
+def fenkai():
+    a=input("请输入：")
     b=len(a)
     for i in range(b):
         print(a[i])
 
-if __name__=="__main__":
-    fnakai()        
+
+
+
+
+
+
+
+
+
+
+if gongnen=="fenkai":
+    fenkai()        
